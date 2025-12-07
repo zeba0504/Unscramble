f1eafe85f38a0b4fc4a18b4c78d724e9a820566f
Zeba <saidzodazebida@gmail.com>
2025-12-07 20:32:40 +0300

    seventh

--- app/src/test/java/com/example/unscramble/ui/test/GameViewModelTest.kt
+++ app/src/test/java/com/example/unscramble/ui/test/GameViewModelTest.kt
@@ -3,6 +3,9 @@ package com.example.unscramble.ui.test
 import org.junit.Test
 import com.example.unscramble.ui.GameViewModel
 import com.example.unscramble.data.getUnscrambledWord
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import com.example.unscramble.data.SCORE_INCREASE
 
 
 class GameViewModelTest {
@@ -15,5 +18,13 @@ class GameViewModelTest {
 
         viewModel.updateUserGuess(correctPlayerWord)
         viewModel.checkUserGuess()
+
+        currentGameUiState = viewModel.uiState.value
+        assertFalse(currentGameUiState.isGuessedWordWrong)
+        assertEquals(20, currentGameUiState.score)
+        assertEquals(SCORE_AFTER_FIRST_CORRECT_ANSWER, currentGameUiState.score)
+    }
+    companion object {
+        private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
     }
 }
