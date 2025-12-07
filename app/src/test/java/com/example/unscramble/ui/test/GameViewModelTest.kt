319cfd9735d19ab989dba6fdffb863f80bc0aee0
Zeba <saidzodazebida@gmail.com>
2025-12-07 20:45:55 +0300

    tenth

--- app/src/test/java/com/example/unscramble/ui/test/GameViewModelTest.kt
+++ app/src/test/java/com/example/unscramble/ui/test/GameViewModelTest.kt
@@ -15,21 +15,22 @@ class GameViewModelTest {
     private val viewModel = GameViewModel()
 
     @Test
-    fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset() {
-        var currentGameUiState = viewModel.uiState.value
-        val correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+    fun gameViewModel_Initialization_FirstWordLoaded() {
+        val gameUiState = viewModel.uiState.value
+        val unScrambledWord = getUnscrambledWord(gameUiState.currentScrambledWord)
 
-        viewModel.updateUserGuess(correctPlayerWord)
-        viewModel.checkUserGuess()
+        // Assert that current word is scrambled.
+        assertNotEquals(unScrambledWord, gameUiState.currentScrambledWord)
+        // Assert that current word count is set to 1.
+        assertTrue(gameUiState.currentWordCount == 1)
+        // Assert that initially the score is 0.
+        assertTrue(gameUiState.score == 0)
+        // Assert that the wrong word guessed is false.
+        assertFalse(gameUiState.isGuessedWordWrong)
+        // Assert that game is not over.
+        assertFalse(gameUiState.isGameOver)
+    }
 
-        currentGameUiState = viewModel.uiState.value
-        assertFalse(currentGameUiState.isGuessedWordWrong)
-        assertEquals(20, currentGameUiState.score)
-        assertEquals(SCORE_AFTER_FIRST_CORRECT_ANSWER, currentGameUiState.score)
-    }
-    companion object {
-        private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
-    }
 
     @Test
     fun gameViewModel_IncorrectGuess_ErrorFlagSet() {
@@ -47,21 +48,22 @@ class GameViewModelTest {
     }
 
     @Test
-    fun gameViewModel_Initialization_FirstWordLoaded() {
-        val gameUiState = viewModel.uiState.value
-        val unScrambledWord = getUnscrambledWord(gameUiState.currentScrambledWord)
+    fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset() {
+        var currentGameUiState = viewModel.uiState.value
+        val correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
 
-        // Assert that current word is scrambled.
-        assertNotEquals(unScrambledWord, gameUiState.currentScrambledWord)
-        // Assert that current word count is set to 1.
-        assertTrue(gameUiState.currentWordCount == 1)
-        // Assert that initially the score is 0.
-        assertTrue(gameUiState.score == 0)
-        // Assert that the wrong word guessed is false.
-        assertFalse(gameUiState.isGuessedWordWrong)
-        // Assert that game is not over.
-        assertFalse(gameUiState.isGameOver)
+        viewModel.updateUserGuess(correctPlayerWord)
+        viewModel.checkUserGuess()
+
+        currentGameUiState = viewModel.uiState.value
+        assertFalse(currentGameUiState.isGuessedWordWrong)
+        assertEquals(20, currentGameUiState.score)
+        assertEquals(SCORE_AFTER_FIRST_CORRECT_ANSWER, currentGameUiState.score)
     }
+    companion object {
+        private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
+    }
+
 
     @Test
     fun gameViewModel_AllWordsGuessed_UiStateUpdatedCorrectly() {
@@ -72,7 +74,15 @@ class GameViewModelTest {
             expectedScore += SCORE_INCREASE
             viewModel.updateUserGuess(correctPlayerWord)
             viewModel.checkUserGuess()
+            currentGameUiState = viewModel.uiState.value
+            correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+            // Assert that after each correct answer, score is updated correctly.
+            assertEquals(expectedScore, currentGameUiState.score)
 
         }
+        // Assert that after all questions are answered, the current word count is up-to-date.
+        assertEquals(MAX_NO_OF_WORDS, currentGameUiState.currentWordCount)
+        // Assert that after 10 questions are answered, the game is over.
+        assertTrue(currentGameUiState.isGameOver)
     }
 }
